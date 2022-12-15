5293fdb7c773b0db794acddff38479b1b1cf9af4
Rickert <rickert.goyvaerts@gmail.com>
2022-12-15 14:45:02 +0100

    frontend added and scanner fixed

---
+++ frontend/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+import Scanner from './scanner/Scanner';
+import HomeView from './views/HomeView';
+
+export default function App() {
+  return (
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeView} />
+        <Stack.Screen name="Scanner" component={Scanner} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+  }
+});
