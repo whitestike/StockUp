a54fdd7694ca3e5835986477aca8baa7ef553320
Rickert <rickert.goyvaerts@gmail.com>
2022-12-19 19:15:38 +0100

    barcode endpoint made

--- backend/src/Controller/BarcodeController.php
+++ backend/src/Controller/BarcodeController.php
@@ -11,10 +11,10 @@ class BarcodeController extends AbstractController
     #[Route('/barcode', methods: ['POST'])]
     public function number(Request $request): Response
     {
-        $data = json_decode($request->getContent())->code;
-        return new Response(
-            ["code" => $data],
-            ['Content-Type' => 'application/json;charset=UTF-8']
+        isset(json_decode($request->getContent())->code) ? $data = ['code' => json_decode($request->getContent())->code] : $data = "no code given";
+        return $this->json(
+            $data,
+            headers: ['Content-Type' => 'application/json;charset=UTF-8']
         );
     }
 }
