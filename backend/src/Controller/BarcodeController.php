c578fddca548d44884d1b475e6480227b961b943
Rickert <rickert.goyvaerts@gmail.com>
2022-12-19 19:00:44 +0100

    barcode endpoint

---
+++ backend/src/Controller/BarcodeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller; 
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/barcode', methods: ['POST'])]
+    public function number(Request $request): Response
+    {
+        return new Response(
+            "test"
+        );
+    }
+}
