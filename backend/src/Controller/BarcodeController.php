e4376a12ed49ff45dd103b115e1bbe79eaa05ca8
Rickert <rickert.goyvaerts@gmail.com>
2022-12-19 19:01:46 +0100

    name change

--- backend/src/Controller/BarcodeController.php
+++ backend/src/Controller/BarcodeController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController extends AbstractController
+class BarcodeController extends AbstractController
 {
     #[Route('/barcode', methods: ['POST'])]
     public function number(Request $request): Response
