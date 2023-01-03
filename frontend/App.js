7d110f91c63ffb519c0bc7020897b932a24f4177
Rickert <rickert.goyvaerts@gmail.com>
2023-01-03 11:40:39 +0100

    deploy as apk

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import axios from 'axios';
 
 import Scanner from './scanner/Scanner';
 import HomeView from './views/HomeView';
 import LoginView from './views/LoginView';
-import NotLoggedIn from './views/NotLoggedInView';
 
 export default function App() {
 
@@ -21,7 +17,6 @@ export default function App() {
         <Stack.Screen options={{title: 'StockUp'}} name="Home" component={HomeView} />
         <Stack.Screen options={{title: 'StockUp'}} name="Scanner" component={Scanner} />
         <Stack.Screen options={{title: 'StockUp'}} name="Login" component={LoginView} />
-        <Stack.Screen options={{title: 'StockUp'}} name="NotLoggedIn" component={NotLoggedIn} />
       </Stack.Navigator>
     </NavigationContainer>
   );
